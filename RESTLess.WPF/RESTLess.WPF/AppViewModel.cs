f9c143df2f6c1a65c6065c5ab4998e72b98c13a4
Lee <gould.lee@googlemail.com>
2014-12-09 16:05:50 +0000

    An initial WPF simple version

---
+++ RESTLess.WPF/RESTLess.WPF/AppViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Caliburn.Micro;
+
+using RestSharp;
+
+namespace RESTLess.WPF
+{
+    public class AppViewModel : PropertyChangedBase
+    {
+        private string rawResultsTextBlock;
+        private string url;
+
+        public string UrlTextBox
+        {
+            get { return url; }
+            set
+            {
+                url = value;
+                NotifyOfPropertyChange(() => UrlTextBox);
+            }
+        }
+
+        public string RawResultsTextBlock
+        {
+            get { return rawResultsTextBlock; }
+            set
+            {
+                rawResultsTextBlock = value;
+                NotifyOfPropertyChange(() => RawResultsTextBlock);
+            }
+        }
+
+        public void SendButton()
+        {
+            var uri = new Uri(url);
+            RestClient client = new RestClient(uri.GetLeftPart(UriPartial.Authority));
+
+            var request = new RestRequest(uri, Method.GET);
+
+            client.ExecuteAsync(request,
+                r =>
+                {
+                    RawResultsTextBlock = r.StatusCode + r.Content;
+                });
+        }
+
+    }
+}
