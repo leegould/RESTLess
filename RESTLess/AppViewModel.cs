f6480599d3c1f36fe9d7aa43851a3dd5e31f455c
Lee <gould.lee@googlemail.com>
2014-12-11 15:41:06 +0000

    Added a method selector.

--- RESTLess/AppViewModel.cs
+++ RESTLess/AppViewModel.cs
@@ -9,8 +9,20 @@ namespace RESTLess
 {
     public class AppViewModel : PropertyChangedBase
     {
+        #region Private members
+        
         private string rawResultsTextBlock;
         private string url;
+
+        private bool getChecked;
+        private bool postChecked;
+        private bool putChecked;
+        private bool deleteChecked;
+
+        #endregion
+
+
+        #region Properties
 
         public string UrlTextBox
         {
@@ -19,6 +31,7 @@ namespace RESTLess
             {
                 url = value;
                 NotifyOfPropertyChange(() => UrlTextBox);
+                NotifyOfPropertyChange(() => CanSendButton);
             }
         }
 
@@ -32,23 +45,111 @@ namespace RESTLess
             }
         }
 
+        public bool GetChecked
+        {
+            get { return getChecked; }
+            set
+            {
+                if (value.Equals(getChecked)) return;
+                getChecked = value;
+                NotifyOfPropertyChange(() => GetChecked);
+            }
+        }
+
+        public bool PostChecked
+        {
+            get { return postChecked; }
+            set
+            {
+                if (value.Equals(postChecked)) return;
+                postChecked = value;
+                NotifyOfPropertyChange(() => PostChecked);
+            }
+        }
+
+        public bool PutChecked
+        {
+            get { return putChecked; }
+            set
+            {
+                if (value.Equals(putChecked)) return;
+                putChecked = value;
+                NotifyOfPropertyChange(() => PutChecked);
+            }
+        }
+
+        public bool DeleteChecked
+        {
+            get { return deleteChecked; }
+            set
+            {
+                if (value.Equals(deleteChecked)) return;
+                deleteChecked = value;
+                NotifyOfPropertyChange(() => DeleteChecked);
+            }
+        }
+
+        public bool CanSendButton
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(UrlTextBox);
+            }
+        }
+
+        #endregion
+
+
+        #region Public Methods
+        
         public void SendButton()
         {
             var uri = new Uri(UrlTextBox);
             RestClient client = new RestClient(uri.GetLeftPart(UriPartial.Authority));
 
-            var request = new RestRequest(uri, Method.GET);
+            var method = GetMethod();
+
+            var request = new RestRequest(uri, method);
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
+
+            RawResultsTextBlock = "Loading " + request.Method + " " + client.BaseUrl + request.Resource + "\n";
             
             client.ExecuteAsync(request,
                 r =>
                 {
                     stopWatch.Stop();
-                    RawResultsTextBlock = stopWatch.ElapsedMilliseconds.ToString() + "|" + r.StatusCode + "|" + r.Content;
+                    RawResultsTextBlock += "Elapsed: " + stopWatch.ElapsedMilliseconds.ToString() + "ms | StatusCode: " + r.ResponseStatus + " " + r.StatusCode + " | Content:" + r.Content;
                 });
         }
 
+        #endregion
+
+
+        #region Private Methods
+
+        private Method GetMethod()
+        {
+            if (GetChecked)
+            {
+                return Method.GET;
+            }
+            if (PostChecked)
+            {
+                return Method.POST;
+            }
+            if (PutChecked)
+            {
+                return Method.PUT;
+            }
+            if (DeleteChecked)
+            {
+                return Method.DELETE;
+            }
+            return Method.GET;
+        }
+
+        #endregion
     }
 }
