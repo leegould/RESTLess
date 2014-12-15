46821e9cea2c3ad867c41a71137bcf9942bcb36f
Lee <gould.lee@googlemail.com>
2014-12-15 10:15:01 +0000

    Window manager

---
+++ RESTLess/AppWindowManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+using Caliburn.Micro;
+
+namespace RESTLess
+{
+    public class AppWindowManager : WindowManager
+    {
+        protected override Window EnsureWindow(object model, object view, bool isDialog)
+        {
+            var window = base.EnsureWindow(model, view, isDialog);
+
+            window.SizeToContent = SizeToContent.Manual;
+
+            window.Width = 400;
+            window.Height = 400;
+
+            return window;
+        }
+    }
+}
