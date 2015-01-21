fab3e148cd7ec3d4884988191a904a095fe7bcbb
Lee <gould.lee@googlemail.com>
2015-01-21 13:38:29 +0000

    Some indexes

---
+++ RESTLess/Indexes/RequestsGroupedAll.cs
@@ -0,0 +1,39 @@
+﻿using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace RESTLess.Indexes
+{
+	public class RequestsGroupedAll : AbstractIndexCreationTask
+	{
+		public override string IndexName
+		{
+			get
+			{
+				return "Requests/Grouped/All";
+			}
+		}
+		public override IndexDefinition CreateIndexDefinition()
+		{
+			return new IndexDefinition
+			{
+				Map = @"from request in docs.Requests
+				select new {
+					Id = request.Id,
+					Url = request.Url,
+					Path = request.Path
+				}",
+				
+				Reduce = @"from result in results
+				group result by new {
+					Url = result.Url,
+					Path = result.Path
+				} into g
+				select new {
+					Id = g.Max(x => x.Id),
+					Url = g.Key.Url,
+					Path = g.Key.Path
+				}"
+			};
+		}
+	}
+}
---
+++ RESTLess/Indexes/Responses_All.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+using Raven.Client.Indexes;
+
+using RESTLess.Models;
+
+namespace RESTLess.Indexes
+{
+    public class Responses_All: AbstractIndexCreationTask<Response>
+    {
+        public Responses_All()
+        {
+            Map = responses => from response in responses
+                            select new
+                            {
+                                response.Id,
+                                response.When,
+                                response.ResponseUri,
+                                response.StatusCode,
+                                response.StatusCodeDescription,
+                                response.Content,
+                                response.Headers
+                            };
+        }
+    }
+}
