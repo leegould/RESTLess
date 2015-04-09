aa04c467561824e34c66396937b89dbbfea46342
Lee <gould.lee@googlemail.com>
2015-04-09 10:10:15 +0100

    missing file

---
+++ RESTLess/Extensions/DocumentSessionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+using Raven.Client;
+
+namespace RESTLess.Extensions
+{
+    public static class DocumentSessionExtensions
+    {
+        public static void ClearDocuments<T>(this IDocumentSession session)
+        {
+            var objects = session.Query<T>().ToList();
+            while (objects.Any())
+            {
+                foreach (var obj in objects)
+                {
+                    session.Delete(obj);
+                }
+
+                session.SaveChanges();
+            }
+        }
+    }
+}
