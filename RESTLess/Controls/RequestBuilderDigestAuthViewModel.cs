b8693f39b24c3f0d846e02b76db84e30c54942f2
Lee Gould <gould.lee@googlemail.com>
2015-05-26 10:27:11 +0100

    Adding new control for digest auth.

---
+++ RESTLess/Controls/RequestBuilderDigestAuthViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Caliburn.Micro;
+using RESTLess.Models;
+using RESTLess.Models.Interface;
+using RESTLess.Models.Messages;
+
+namespace RESTLess.Controls
+{
+    public class RequestBuilderDigestAuthViewModel : Screen, ITabItem //, IHandle<CreateRequestMessage>, IHandle<ClearMessage>
+    {
+        private readonly IEventAggregator eventAggregator;
+
+        public RequestBuilderDigestAuthViewModel(IEventAggregator eventAggregator)
+        {
+            DisplayName = "Basic Auth";
+            this.eventAggregator = eventAggregator;
+            eventAggregator.Subscribe(this);
+        }
+    }
+}
