6b7c57f70441ba4db1e7b33b6b7409af8d532cd2
Lee Gould <gould.lee@googlemail.com>
2015-05-27 11:14:33 +0100

    properties for digest auth

--- RESTLess/Controls/RequestBuilderDigestAuthViewModel.cs
+++ RESTLess/Controls/RequestBuilderDigestAuthViewModel.cs
@@ -15,9 +15,121 @@ namespace RESTLess.Controls
     {
         private readonly IEventAggregator eventAggregator;
 
+        private string usernameTextBox;
+
+        private string passwordTextBox;
+
+        private string realmTextBox;
+
+        private string nonceTextBox;
+
+        private string algoTextBox;
+
+        private string qopTextBox;
+
+        private int nonceCount;
+
+        private string clientNonceTextBox;
+
+        private string opaqueTextBox;
+
+        #region Properties
+
+        public string UsernameTextBox
+        {
+            get { return usernameTextBox; }
+            set
+            {
+                usernameTextBox = value;
+                NotifyOfPropertyChange(() => UsernameTextBox);
+            }
+        }
+
+        public string PasswordTextBox
+        {
+            get { return passwordTextBox; }
+            set
+            {
+                passwordTextBox = value;
+                NotifyOfPropertyChange(() => PasswordTextBox);
+            }
+        }
+
+        public string RealmTextBox
+        {
+            get { return realmTextBox; }
+            set
+            {
+                realmTextBox = value;
+                NotifyOfPropertyChange(() => RealmTextBox);
+            }
+        }
+
+        public string NonceTextBox
+        {
+            get { return nonceTextBox; }
+            set
+            {
+                nonceTextBox = value;
+                NotifyOfPropertyChange(() => NonceTextBox);
+            }
+        }
+
+        public string AlgoTextBox
+        {
+            get { return algoTextBox; }
+            set
+            {
+                algoTextBox = value;
+                NotifyOfPropertyChange(() => AlgoTextBox);
+            }
+        }
+
+        public string QopTextBox
+        {
+            get { return qopTextBox; }
+            set
+            {
+                qopTextBox = value;
+                NotifyOfPropertyChange(() => QopTextBox);
+            }
+        }
+
+        public int NonceCount
+        {
+            get { return nonceCount; }
+            set
+            {
+                nonceCount = value;
+                NotifyOfPropertyChange(() => NonceCount);
+            }
+        }
+
+        public string ClientNonceTextBox
+        {
+            get { return clientNonceTextBox; }
+            set
+            {
+                clientNonceTextBox = value;
+                NotifyOfPropertyChange(() => ClientNonceTextBox);
+            }
+        }
+
+        public string OpaqueTextBox
+        {
+            get { return opaqueTextBox; }
+            set
+            {
+                opaqueTextBox = value;
+                NotifyOfPropertyChange(() => OpaqueTextBox);
+            }
+        }
+
+        #endregion
+
         public RequestBuilderDigestAuthViewModel(IEventAggregator eventAggregator)
         {
-            DisplayName = "Basic Auth";
+            DisplayName = "Digest Auth";
             this.eventAggregator = eventAggregator;
             eventAggregator.Subscribe(this);
         }
